9be3d4602cd77545c6792ef23df1a0a43267a013
Lars Egholm Pedersen <LSPD@simcorp.com>
2019-08-01 15:29:27 +0200

    Move core func to library

--- Lab2.Interfaces/Lab2.Interfaces.ConsoleApp/Program.cs
+++ Lab2.Interfaces/Lab2.Interfaces.ConsoleApp/Program.cs
@@ -1,47 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Lab2.Interfaces.ConsoleApp {
+﻿namespace Lab2.Interfaces.ConsoleApp {
     static class Program {
         static void Main(string[] args) {
 
             var output = new ConsoleOutput();
-
-            var playbackOptions = new List<IPlayback> {
-                new AppleHeadset(output),
-                new PhoneSpeaker(output),
-                new SamsungHeadset(output),
-                new UnofficialHeadset(output)
-            };
-
-            var displayOptions = new List<IDisplay> {
-                new InbuiltDisplay(output),
-                new CastDisplay(output)
-            };
-
-            var playbackChoice = ChooseFromList("playback components", playbackOptions);
-            var displayChoice = ChooseFromList("display component", displayOptions);
-
-            Console.WriteLine($"{playbackOptions[playbackChoice].Name()}, {displayOptions[displayChoice].Name()} selected.");
-
-            var mobile = new SimCorpMobile {
-                PlaybackComponent = playbackOptions[playbackChoice],
-                DisplayComponent = displayOptions[displayChoice]
-            };
-
+            var mobile = new MobileBuilder(output).Mobile;
             mobile.Play("");
             mobile.Display("");
         }
-
-        private static int ChooseFromList(string choicesTitle, IReadOnlyList<INamedClass> choices) {
-            Console.WriteLine($"Select {choicesTitle} index");
-            for (var i = 0; i < choices.Count; i++) {
-                Console.WriteLine($"{i}: {choices[i].Name()}");
-            }
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-
-
     }
 }
---
+++ Lab2.Interfaces/Lab2.Interfaces/MobileBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Lab2.Interfaces.ConsoleApp;
+
+namespace Lab2.Interfaces {
+    public class MobileBuilder {
+
+        private readonly IOutput Output;
+        public Mobile Mobile { get; }
+
+        public MobileBuilder(IOutput output) {
+
+            Output = output;
+
+            var playbackOptions = new List<IPlayback> {
+                new AppleHeadset(output),
+                new PhoneSpeaker(output),
+                new SamsungHeadset(output),
+                new UnofficialHeadset(output)
+            };
+
+            var displayOptions = new List<IDisplay> {
+                new InbuiltDisplay(output),
+                new CastDisplay(output)
+            };
+
+            var playbackChoice = ChooseFromList("playback components", playbackOptions);
+            var displayChoice = ChooseFromList("display component", displayOptions);
+
+            Output.WriteLine($"{playbackOptions[playbackChoice].Name()}, {displayOptions[displayChoice].Name()} selected.");
+
+            Mobile = new SimCorpMobile {
+                PlaybackComponent = playbackOptions[playbackChoice],
+                DisplayComponent = displayOptions[displayChoice]
+            };
+        }
+
+        private int ChooseFromList(string choicesTitle, IReadOnlyList<INamedClass> choices) {
+            Output.WriteLine($"Select {choicesTitle} index");
+            for (var i = 0; i < choices.Count; i++) {
+                Output.WriteLine($"{i}: {choices[i].Name()}");
+            }
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}

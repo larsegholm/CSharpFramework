ef0a520cec36a00ac975789cbf7e396396762ffd
Lars Egholm Pedersen <LSPD@simcorp.com>
2019-08-06 11:07:27 +0200

    Added basic winform / no functionality

---
+++ Lab2.Interfaces/Lab2.Interfaces.WinForm/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab2.Interfaces.WinForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}

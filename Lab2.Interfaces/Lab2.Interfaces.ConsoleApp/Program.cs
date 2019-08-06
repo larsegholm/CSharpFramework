56a9120641a4351f97192d961d01bf27f3d5d287
Lars Egholm Pedersen <LSPD@simcorp.com>
2019-08-06 11:22:07 +0200

    Move console interactions to console app

--- Lab2.Interfaces/Lab2.Interfaces.ConsoleApp/Program.cs
+++ Lab2.Interfaces/Lab2.Interfaces.ConsoleApp/Program.cs
@@ -1,11 +1,34 @@
-﻿namespace Lab2.Interfaces.ConsoleApp {
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2.Interfaces.ConsoleApp {
     static class Program {
         static void Main(string[] args) {
 
             var output = new ConsoleOutput();
-            var mobile = new MobileBuilder(output).Mobile;
+            var mobileOptions = new MobileOptions(output);
+            var playbackOptions = mobileOptions.PlaybackOptions;
+            var displayOptions = mobileOptions.DisplayOptions;
+
+            var playbackChoice = playbackOptions[ChooseFromList("playback components", playbackOptions, output)];
+            var displayChoice = displayOptions[ChooseFromList("display component", displayOptions, output)];
+
+            output.WriteLine($"{ playbackChoice.Name()}, {displayChoice.Name()} selected.");
+                
+            var mobile = new SimCorpMobile {
+                PlaybackComponent = playbackChoice,
+                DisplayComponent = displayChoice
+            };
             mobile.Play("");
             mobile.Display("");
+        }
+
+        private static int ChooseFromList(string choicesTitle, IReadOnlyList<INamedClass> choices, IOutput output) {
+            output.WriteLine($"Select {choicesTitle} index");
+            for (var i = 0; i < choices.Count; i++) {
+                output.WriteLine($"{i}: {choices[i].Name()}");
+            }
+            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
---
+++ Lab2.Interfaces/Lab2.Interfaces/MobileOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Lab2.Interfaces {
+    public class MobileOptions {
+        public readonly List<IPlayback> PlaybackOptions;
+        public readonly List<IDisplay> DisplayOptions;
+
+        public MobileOptions(IOutput output) {
+
+            PlaybackOptions = new List<IPlayback> {
+                new AppleHeadset(output),
+                new PhoneSpeaker(output),
+                new SamsungHeadset(output),
+                new UnofficialHeadset(output)
+            };
+
+            DisplayOptions = new List<IDisplay> {
+                new InbuiltDisplay(output),
+                new CastDisplay(output)
+            };
+        }
+    }
+}

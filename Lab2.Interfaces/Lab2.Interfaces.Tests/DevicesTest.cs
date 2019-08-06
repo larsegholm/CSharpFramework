f6c154b236114b611fa4589c461fb754052d20c7
Lars Egholm Pedersen <LSPD@simcorp.com>
2019-08-06 13:23:44 +0200

    Added test suite

---
+++ Lab2.Interfaces/Lab2.Interfaces.Tests/DevicesTest.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Lab2.Interfaces.Tests {
+
+    public class DevicesTest {
+
+        private Mock<IOutput> OutputMock;
+
+        [SetUp]
+        public void Setup() {
+            OutputMock = new Mock<IOutput>();
+            OutputMock.Setup(output => output.WriteLine(It.IsAny<string>()));
+        }
+
+        [Test]
+        public void AppleHeadsetReturnsNameInNameMethod() {
+            var appleHeadSet = new AppleHeadset(OutputMock.Object);
+
+            var name = appleHeadSet.Name();
+
+            Assert.AreEqual(name, "AppleHeadset");
+        }
+
+        [Test]
+        public void AppleHeadsetOutputsNameWhenPlayed() {
+            var appleHeadSet = new AppleHeadset(OutputMock.Object);
+
+            appleHeadSet.Play("");
+
+            OutputMock.Verify(output => output.WriteLine(appleHeadSet.Name() + " sound"));
+        }
+
+        [Test]
+        public void CastDisplayReturnsNameInNameMethod() {
+            var castDisplay = new CastDisplay(OutputMock.Object);
+
+            var name = castDisplay.Name();
+
+            Assert.AreEqual(name, "CastDisplay");
+        }
+
+        [Test]
+        public void CastDisplayShowsNameNameWhenDisplayed() {
+            var castDisplay = new CastDisplay(OutputMock.Object);
+
+            castDisplay.Display("");
+
+            OutputMock.Verify(output => output.WriteLine("Displaying from " + castDisplay.Name()));
+        }
+
+
+        // Etc. etc.
+    }
+}
---
+++ Lab2.Interfaces/Lab2.Interfaces.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab2.Interfaces.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2.Interfaces.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("062fd485-7579-45aa-a5b6-5ec673e4040f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
